f84751b4dcbf3755b5cd91aa4f78b095357a16f3
Thomas Salas <tjsalas18@sbcglobal.net>
2018-07-03 12:18:39 -0400

    Site is split inot 10 sections that will update when data is changed

--- src/footer.js
+++ src/footer.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 
-class NavigationBar extends Component{
+class Footer extends Component{
 render(){
     return(
-        <div className="float-right nav-links">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Log in</a>
-      </div>
+        <footer className="expanded row">
+        <h6>&copy; 2017 Erebor Industries</h6>
+        <h6 className="float-right">All rights reserved</h6>
+      </footer>
     )
   }
 }
 
-export default NavigationBar
+export default Footer
